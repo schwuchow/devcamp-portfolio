db824e156956038de6f5b3894e1c5b513f3a182b
Judali <schwuchow@cip.ifi.lmu.de>
2018-10-02 20:13:12 +0200

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get "portfolios/:id", to: "portfolios#show", as: "portfolio_show"
 
   get "about-me", to: "pages#about"
   get "contact", to: "pages#contact"
