8aecd3fcf1f91bc1a151a4d364d4b039868945f1
Judali <schwuchow@cip.ifi.lmu.de>
2018-10-03 08:42:09 +0200

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  get "portfolios/:id", to: "portfolios#show", as: "portfolio_show"
+  get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
 
   get "about-me", to: "pages#about"
   get "contact", to: "pages#contact"
