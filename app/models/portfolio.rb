04070b1656707b4acf9238eee1ad1c0d75f5f884
Judali <schwuchow@cip.ifi.lmu.de>
2018-10-14 11:04:01 +0200

    Implemented git defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,11 @@ class Portfolio < ApplicationRecord
 
   #option 2
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||=  "https://via.placeholder.com/600x400"
+    self.thumb_image ||=  "https://via.placeholder.com/350x200"
+  end
 end
